c0252fa22f5631d006031f1cb22bb8e39a30a3ce
dyfolab <dyfolabs@gmail.com>
2026-05-08 14:16:02 +0530

    add the company logo

--- my_gis_project/celery.py
+++ my_gis_project/celery.py
@@ -1,8 +1,8 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_gis.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_gis_project.settings')
 
-app = Celery('web_gis')
+app = Celery('my_gis_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
--- my_gis_project/settings.py
+++ my_gis_project/settings.py
@@ -80,6 +80,7 @@ USE_TZ = True
 
 # Static & Media files
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
